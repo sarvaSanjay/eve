81b0968359ff9b2f01e4fb7059ba0d07df4dd37a
Aina Merchant <ainamerchant10@gmail.com>
2024-08-04 05:32:49 -0400

    Charts + Style

--- eve_frontend/src/app/page.tsx
+++ eve_frontend/src/app/page.tsx
@@ -19,11 +19,30 @@ const StartCommandPage = () => {
     };
   }, []);
 
-  const handleStartRobot = () => {
+  const handleStartRobot = async () => {
     if (socket) {
+      // // Emit 'connect_browser' event and wait for acknowledgment
+      // await new Promise<void>((resolve, reject) => {
+      //   socket.emit('connect_browser', { robot_id: 'robot123' }, (response) => {
+      //     console.log(response)
+      //     if (response) {
+      //       resolve();
+      //     } else {
+      //       reject(new Error('Failed to connect browser'));
+      //     }
+      //   });
+      // });
+
+      socket.emit('connect_browser', { robot_id: 'robot123' });
+      socket.on('robot_connected', (data) => {
+        console.log(data.message);
+      });
+
+      // Emit 'send_command' event
       socket.emit('send_command', { command: 'start_robot' });
 
-      socket.once('send_echo_report', (data) => {
+      // Listen for 'send_eco_report' event and navigate
+      socket.on('send_eco_report', (data) => {
         // Convert data to a JSON string and navigate
         const queryString = `data=${encodeURIComponent(JSON.stringify(data))}`;
         router.push(`/eco-report?${queryString}`);
--- eve_frontend/src/components/Energy.tsx
+++ eve_frontend/src/components/Energy.tsx
@@ -17,39 +17,62 @@ const data = {
     },
   ],
 };
-const InfoCard = ({ icon: Icon, title, overallRating, overallJustification, subsections, bgcolor = '#98BF64', 
-  iconcolor = '#fff' }) => {
+const InfoCard = ({ icon: Icon, title, overallRating, overallJustification, sub_1, sub_1_rating, sub_1_just, sub_2, sub_2_rating, sub_2_just}) => {
   return (
-    <Paper elevation={3} sx={{ padding: '20px', backgroundColor: bgcolor }}>
+    <Paper elevation={3} sx={{ padding: '20px' }} className={title === "INDOOR AIR QUALITY"?'#bg-gradient-to-r from-grey-900 via-grey-100 to-grey-900':
+    'bg-gradient-to-r from-green-400 via-green-100 to-green-400'}>
       <Grid container spacing={3}>
+        {/* Icon Column */}
         <Grid item xs={4} display="flex" justifyContent="center" alignItems="center">
-          <Icon sx={{ fontSize: 150, color: iconcolor }} />
+          <Icon sx={{ fontSize: 150, color: '#fff' }} />
         </Grid>
-        <Grid item xs={8}>
-        <Box>
-          <Typography variant="h5" component="h2" className='font-economica' gutterBottom>
-            {title}
-          </Typography>
-          {Object.keys(subsections).map(subsection => (
-            <div key={subsection}>
-              <Typography variant="body1" className='font-economica'>
-                <strong>{subsection} Rating:</strong> {subsections[subsection].rating}
+
+        {/* Text Column */}
+        <Grid item xs={4}>
+          <Box sx={{ maxWidth: '400px' }}>
+            <Typography variant="h5" component="h2" className='font-economica' gutterBottom>
+              {title}
+            </Typography>
+
+            <div key={title}>
+              <Typography variant="h6" className='font-economica'>
+                <strong>{sub_1}</strong>
               </Typography>
               <Typography variant="body1" className='font-economica'>
-                <strong>{subsection} Justification:</strong> {subsections[subsection].justification}
+                <strong>{sub_1} Rating:</strong> {sub_1_rating}
+              </Typography>
+              <Typography variant="body1" className='font-economica'>
+                <strong>{sub_1} Justification:</strong> {sub_1_just}
+              </Typography>
+              <Typography variant="h6" className='font-economica'>
+                <strong>{sub_2}</strong>
+              </Typography>
+              <Typography variant="body1" className='font-economica'>
+                <strong>{sub_2} Rating:</strong> {sub_2_rating}
+              </Typography>
+              <Typography variant="body1" className='font-economica'>
+                <strong>{sub_2} Justification:</strong> {sub_2_just}
               </Typography>
             </div>
-          ))}
-          <Typography variant="body1" className='font-economica'>
-            <strong>Overall Rating:</strong> {overallRating}
-          </Typography>
-          <Typography variant="body1" className='font-economica'>
-            <strong>Overall Justification:</strong> {overallJustification}
-          </Typography>
-        </Box>
+
+            <Typography variant="body1" className='font-economica'>
+              <strong>Overall Rating:</strong> {overallRating}
+            </Typography>
+            <Typography variant="body1" className='font-economica'>
+              <strong>Overall Justification:</strong> {overallJustification}
+            </Typography>
+          </Box>
+        </Grid>
+
+        {/* Graph Column */}
+        <Grid item xs={4} display="flex" justifyContent="center" alignItems="center">
+          <PieChart data={data} />
+        </Grid>
       </Grid>
-    </Grid>
-  </Paper>
+    </Paper>
+  
+
+  
 );
 };
 
